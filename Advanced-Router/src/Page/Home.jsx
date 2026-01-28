134740d7f4a0549139242d1d08ad22c641691eec
Vraj-V <vrajv2108@gmail.com>
2026-01-28 18:38:31 +0530

    Add Home Page

--- Advanced-Router/src/Page/Home.jsx
+++ Advanced-Router/src/Page/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h3>Home</h3>
+        <h3>Home Page</h3>
+        <div className='text-container'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, aut.
+          Voluptatibus officiis quod nemo quia, sequi voluptas quae natus quibusdam quos.</p>
+          <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, aut. Voluptatibus officiis quod nemo quia, sequi voluptas quae natus quibusdam quos.</h5>
+        </div>
     </div>
   )
 }
