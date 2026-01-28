3d9a55cffaf19c13b2059a9ac5dfecaf7d892cba
Vraj-V <vrajv2108@gmail.com>
2026-01-28 18:40:45 +0530

    Added About page

--- Advanced-Router/src/Page/About.jsx
+++ Advanced-Router/src/Page/About.jsx
@@ -10,10 +10,14 @@ let navigate = useNavigate();
     <div>
 
     <Buttons />
-
-
-
         <h3>About</h3>
+        <div className='text-center'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, aut.
+          Voluptatibus officiis quod nemo quia, sequi voluptas quae natus quibusdam quos.</p>
+          <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, aut. Voluptatibus officiis quod nemo quia, sequi voluptas quae natus quibusdam quos.</h5>
+
+        </div>
+        <button onClick={()=>navigate('/')} className='bg-green-400 text-white px-4 py-2 rounded-md'>Go to Home</button>
     </div>
   )
 }
