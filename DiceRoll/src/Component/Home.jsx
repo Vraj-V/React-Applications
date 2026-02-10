fb72506abc462d223664081f2cb44b5a96f062db
Vraj Valand <123581157+Vraj-V@users.noreply.github.com>
2026-02-10 17:56:27 +0000

    New project Dice ROll Game

---
+++ DiceRoll/src/Component/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from './Home.module.css'
+const Home = () => {
+  return (
+    <div className={style.container}>
+        <div>
+            <img src="" alt="" />
+
+        </div>
+        <h1 className={style.title}>Dice Roll</h1>
+        <button className={style.playButton}>Play</button>
+    </div>
+  )
+}
+
+export default Home
