5bcfb5858f3ddac2c1db4cebfabc49e8bdb60f25
Vraj Valand <123581157+Vraj-V@users.noreply.github.com>
2026-02-11 16:57:04 +0000

    Begineer Course

---
+++ Begineer_React_course/src/Components/React_Fundamental/Jsx_Syntax.jsx
@@ -0,0 +1,11 @@
+const Jsx_Syntax = () => {
+    return (
+        <div>
+            <h1>JSX Syntax</h1>
+            <p>JSX stands for JavaScript XML. It allows us to write HTML in React. JSX makes it easier to write and add HTML in React.</p>
+            <h2>JSX Syntax Rules</h2>
+
+            </div>
+    )
+}
+export default Jsx_Syntax;
