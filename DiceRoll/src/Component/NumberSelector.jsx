1ec0d039bd051137745bb0ada182f47cfdde0859
Vraj Valand <123581157+Vraj-V@users.noreply.github.com>
2026-02-12 12:32:15 +0000

    Add the selector

--- DiceRoll/src/Component/NumberSelector.jsx
+++ DiceRoll/src/Component/NumberSelector.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import styled from "styled-components";
+
 const NumberSelector =()=>{
+    const arrNum = [1,2,3,4,5,6];
+    const [selectedNumber, setSelectedNumber] = useState(0);
+    console.log(selectedNumber);
     return(
         <div>
-            <h1>NumberSelector.</h1>
+            {arrNum.map((value,i)=>{
+            return <Box 
+            isSelected={selectedNumber === value}
+            key={i} onClick={()=>setSelectedNumber(value)}>{value}</Box>
+            })}
         </div>
     )
 }
 export default NumberSelector;
+
+const Box = styled.div`
+    height: 72px; 
+    width: 72px;
+    border: 1px solid black;
+    display: grid;
+    place-items: center;
+    font-size: 24px;
+    font-weight: 700;
+    cursor: pointer;
+    background-color: ${(props)=> props.isSelected ? 'black' : 'white'};
+    color: ${(props)=> !props.isSelected ? 'black' :'white'};
+    &:hover{
+        background-color: black;
+        color:white;
+    }
+`
--- DiceRoll/src/pages/Game.jsx
+++ DiceRoll/src/pages/Game.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import style from './Game.module.css'
+import TotalScore from '../Component/TotalScore';
+import NumberSelector from '../Component/NumberSelector';
 const Game = ({flag, setFlag}) => {
   console.log(flag);
   const returnHome = () => {
@@ -14,10 +16,10 @@ const Game = ({flag, setFlag}) => {
       <h1>Game Screen</h1>
       <button onClick={returnHome} className={style.playbutton}>X</button>
     </div>
-    <div className={style.score}>
-      <h1>0</h1>
-      <p>Total Score</p>
-    </div>
+    <main>
+    <TotalScore />
+    <NumberSelector />
+    </main>
     </div>
     </>
   )
